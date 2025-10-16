d42583c2c137e10208ca6e542813340f5959be0f
rayhanfatinfirjatullah <rayhanfatin2018@gmail.com>
2025-10-16 10:36:32 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, repellendus modi, ullam sunt exercitationem reprehenderit officia accusantium praesentium facere quisquam sequi tempore id quaerat dolor ea ex tempora consequuntur nam?</p>
+  <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+  </ul>
 </body>
 </html>
